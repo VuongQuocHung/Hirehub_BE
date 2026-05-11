638b306369b7ba03a8bf4446feb80bfcbd3e4f93
Vương Quốc Hưng <qhung8a@gmail.com>
2026-05-11 15:41:10 +0700

    Backend lưu dữ liệu vào db

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,10 +1,47 @@
 import { Request, Response } from "express";
+import AccountUser from "../models/account-user.model";
+import bcrypt from 'bcryptjs';
 
-export const registerPost = (req: Request, res: Response) => {
-  console.log(req.body);
+export const registerPost = async (req: Request, res: Response) => {
+  try {
+    const {fullName, email, password} = req.body;
+    const existAccount = await AccountUser.findOne({
+      email: email
+    });
 
-  res.json({
-    code: "success",
-    message: "Đăng ký thành công!"
-  });
+    if(existAccount){
+      res.json({
+        code: "error",
+        message: "Email đã tồn tại trong hệ thống!"
+      });
+      return;
+    }
+
+    // Mã hóa mật khẩu với bcryptjs 
+    const salt = await bcrypt.genSalt(10); // Tạo salt - chuỗi ngẫu nhiên có 10 ký tự 
+    const hashedPassword = await bcrypt.hash(password, salt); // Mã hóa mật khẩu
+
+    // console.log("Chạy vào controller");
+
+    const newAccount = new AccountUser({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+      status: "initial"
+    });
+
+    await newAccount.save();
+    
+    // hàm của express: chuyển js sang json và trả về cho frontend json
+    res.json({
+      code: "success",
+      message: "Đăng ký thành công"
+    })
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Đăng ký thất bại"
+    })
+  }
 }
